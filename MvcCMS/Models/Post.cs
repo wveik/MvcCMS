dbd0f1cc734f8e79b604f976f6f57f3e7330a868
wveik <kataev_06@list.ru>
2015-03-08 09:57:34 +0300

    First Commit

---
+++ MvcCMS/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcCMS.Models
+{
+    public class Post
+    {
+        [Display(Name="Slug")]
+        public string Id { get; set; }
+
+        [Display(Name="Title")]
+        public string Title { get; set; }
+
+        [Display(Name="Post Content")]
+        public string Content { get; set; }
+
+        [Display(Name="Date Created")]
+        public DateTime Create { get; set; }
+
+        [Display(Name="Date Published")]
+        public DateTime? Published { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
